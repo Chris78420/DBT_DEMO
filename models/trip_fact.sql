b50262787a8b21b3abf170fba576f20c5b516b4f
Christophe SARAZIN <csarazin@free.fr>
2026-01-30 12:50:07 +0000

    trip_fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,16 @@
+with cte_trips as (
+    select 
+       ride_id,
+       rideable_type,
+       date(to_timestamp(started_at)) as trip_date,
+       start_statio_id as start_station_id,
+       end_station_id,
+       member_csual as member_casual,
+       timestampdiff(second,  to_timestamp(started_at), to_timestamp(ended_at)) as trip_duration_seconds
+
+    from {{ source('demo', 'bike') }}
+    group by all
+    limit 10
+)
+
+select * from cte_trips 
